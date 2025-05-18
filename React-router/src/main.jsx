194a64b06eb2a09be03b65eb59147b954cdcf9ae
amarshri <amarshri6139@gmail.com>
2025-05-18 15:29:17 +0530

    adding files

--- React-router/src/main.jsx
+++ React-router/src/main.jsx
@@ -3,8 +3,51 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import Home from './Components/Home/Home.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './Components/About/About.jsx'
+import Contact from './Components/Contectus/Contect.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+
+    <Route path='' element={<Home/>}/>
+    <Route path='About' element={<About/>}/>
+    <Route path='Contect' element={<Contact/>}/>
+  
+    </Route>
+
+  )
+)
+
+// or
+
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children: [
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"About",
+//         element:<About/>
+//       },
+//       {
+//         path:"Contect",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
